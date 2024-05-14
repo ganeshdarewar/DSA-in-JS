8aad7f82b6a4d165608d12a9b724b91b1758174f
ganeshdarewar <ganeshdarewar@gmail.com>
2024-05-14 11:19:24 +0530

    lecture 3 completed

---
+++ week1/l3.js
@@ -0,0 +1,66 @@
+
+// 2 table
+// const num=2
+// for(let i=1; i<=10; i++){
+//     console.log(`${num} x ${i} = ${num*i}`);
+// }
+
+
+
+// square pattern
+// const n=5
+// for(let row=0; row<n; row++){
+//     let stars=''
+//     for(let col=0; col<n ; col++){
+//         stars +='*'
+//     }
+//     console.log(stars);
+// }
+
+// hollow rectangle
+
+// const n=5; 
+// for(let row=0; row<n; row++){
+//     let stars=''
+//     for(let col=0; col<n; col++){
+//         row ===0 || row===n-1 || col=== n-1 || col===0 ? stars+= '*': stars+= ' '
+//     }
+//     console.log(stars);
+// }
+
+
+// half pyramid
+
+// const n=5
+// for(let row=0; row<n; row++){
+//     stars=''
+//     for(let col=0; col<row+1; col++){
+//         stars += "*"
+//     }
+//     console.log(stars);
+// }
+
+
+// inverted half pyramid
+
+// const n=5
+// for(let row=0; row<n; row++){
+//     let stars=''
+//     for(let col=0; col<n-row; col++){
+//         stars+="*"
+//     }
+//     console.log(stars);
+// }
+
+
+// numeric half
+
+const n=5
+let count=0
+for(let row=0; row<n; row++){
+    let stars =''
+    for(let col=0; col<row+1; col++){
+        stars += count++
+    }
+    console.log(stars);
+}
